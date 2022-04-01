3449395a0b2bcd8cd684dc69fe9548ed0f41bc42
Sam <samsy318417@gmail.com>
2022-04-01 16:48:07 +0800

    test

---
+++ Data/Entity/View/PPSFView.cs
@@ -0,0 +1,20 @@
+﻿namespace SnakeAsianLeague.Data.Entity.View
+{
+    public class PPSFView
+    {
+
+        public string SerialNumber { get; set; }     // NFT 單位編號
+
+        public string NftName { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public double Price { get; set; }
+
+        public DateTime UnBoxingTime { get; set; }
+
+
+
+
+    }
+}
