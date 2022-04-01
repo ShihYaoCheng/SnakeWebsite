893b3820d6390cd655d2a32a5454a93e6e2f74c8
Reila W <ravennreila@gmail.com>
2022-04-01 16:43:27 +0800

    marketPlaceSidebar RWD

--- wwwroot/JavaScript/MarketPlace.js
+++ wwwroot/JavaScript/MarketPlace.js
@@ -1,10 +1,31 @@
 window.heartClick = function () {
     $('.heart, .Filter-heart').click(function(){
+        console.log("heartClick")
         $(this).toggleClass('heartClickRed')
     })
 }
 window.FilterButtonClick = function () {
     $('.Filter-Button').click(function(){
+        console.log('FilterButtonClick')
         $(this).toggleClass('Filter-Button-Click')
     })
+}
+
+window.FilterArrowSlideClick = function () {
+    $('.Filter-Title').click(function(){
+        console.log('FilterArrowSlideClick')
+        $(this).parent().children('.Filter-Options').slideToggle();
+
+    })
+    $('#Filter-mobile').click(function(){
+        console.log('Filter-mobile-Slide')
+        $('.marketPlaceSidebar').slideToggle(); 
+    })
+
+    // if($('.marketPlaceSidebar').length = 1 ){
+    //     $(document).not($('.marketPlaceSidebar')).click(function(){
+    //         $('.marketPlaceSidebar').slideUp();
+    //     });
+    // }
+
 }
