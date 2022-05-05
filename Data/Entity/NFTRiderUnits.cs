7e8b5591f610d2208c77a443e907741baf58776c
YuWei <yuwei4chenquan@gmail.com>
2022-05-05 14:07:11 +0800

    MarketPlace 抓取api nftunit資料

---
+++ Data/Entity/NFTRiderUnits.cs
@@ -0,0 +1,38 @@
+﻿namespace SnakeAsianLeague.Data.Entity
+{
+    public class NFTRiderUnits
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int mintCount { get; set; }
+        /// <summary>
+        /// PPSR
+        /// </summary>
+        public string serialNumber { get; set; }
+        /// <summary>
+        /// 名子
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// 尺寸
+        /// </summary>
+        public double size { get; set; }
+        /// <summary>
+        /// 騎士
+        /// </summary>
+        public string knight { get; set; }
+        /// <summary>
+        /// 蛇蛇
+        /// </summary>
+        public string snake { get; set; }
+        /// <summary>
+        /// 武器
+        /// </summary>
+        public string weapon { get; set; }
+        /// <summary>
+        /// 寵物
+        /// </summary>
+        public string pet { get; set; }
+    }
+}
--- Data/Services/MarketPlace/OptionService.cs
+++ Data/Services/MarketPlace/OptionService.cs
@@ -1,10 +1,27 @@
-﻿using SnakeAsianLeague.Data.Entity;
+﻿using Microsoft.Extensions.Options;
+using RestSharp;
+using SnakeAsianLeague.Data.Entity;
+using SnakeAsianLeague.Data.Entity.Config;
 using SnakeAsianLeague.Data.Paging;
+using System.Net;
+using System.Text.Json;
 
 namespace SnakeAsianLeague.Data.Services.MarketPlace
 {
     public class OptionService
     {
+        private ExternalServers externalServersConfig;
+        private readonly RestClient ServerClient;
+
+        public OptionService(IOptions<ExternalServers> myConfiguration,HttpClient httpClient)
+        {
+            externalServersConfig = myConfiguration.Value;
+            ServerClient = new RestClient(externalServersConfig.UserServer);
+        }
+
+
+
+
         public List<string> RareList = new List<string>() { "N", "R", "SR", "SSR", "UR", "UUR" };
 
         public List<string> ClassList = new List<string>() { "Land", "Water", "Wind", "Fire" };
@@ -46,15 +63,21 @@ namespace SnakeAsianLeague.Data.Services.MarketPlace
         /// <returns></returns>
         public async Task<PagedList<NFTData>> GetNFTDataPageList( int PageNumber , int PageSize)
         {
+
+
+            List<NFTRiderUnits> nFTRiders = await GetNFTRiderUnits();
+
+
+            
+
             datas = new List<NFTData>();
+
 
             Random myObject = new Random();
 
 
-
-            for (int i = 0; i < 30; i++)
+            for (int i = 0; i < nFTRiders.Count; i++)
             {
-
                 int value = myObject.Next(1, 1000);
                 int RareInt = value % RareList.Count();
                 int ClassInt = value % ClassList.Count();
@@ -62,11 +85,11 @@ namespace SnakeAsianLeague.Data.Services.MarketPlace
                 int CountryInt = value % RareList.Count();
 
                 NFTData data = new NFTData();
-                data.Number = value;
-                data.Name = value.ToString() + "-" + ProfessionList[ProfessionInt].ToString();
+                data.Number = value.ToString();
+                data.Name = nFTRiders[i].name;
                 data.Price = myObject.Next(value, value * 10);
                 data.USD = myObject.Next(value, value * 10) * 30;
-                data.ImgPath = "/images/MarketPlace/ERNC.png";
+                data.ImgPath = string.Format(@"https://storage.googleapis.com/pps-nft/ppsr/{0}.png", nFTRiders[i].serialNumber);
                 data.Rare = RareList[RareInt].ToString();
                 data.Class = ClassList[ClassInt].ToString();
                 data.Profession = ProfessionList[ProfessionInt].ToString();
@@ -88,6 +111,47 @@ namespace SnakeAsianLeague.Data.Services.MarketPlace
 
                 datas.Add(data);
             }
+
+
+
+
+
+            //for (int i = 0; i < 30; i++)
+            //{
+
+            //    int value = myObject.Next(1, 1000);
+            //    int RareInt = value % RareList.Count();
+            //    int ClassInt = value % ClassList.Count();
+            //    int ProfessionInt = value % RareList.Count();
+            //    int CountryInt = value % RareList.Count();
+
+            //    NFTData data = new NFTData();
+            //    data.Number = value.ToString();
+            //    data.Name = value.ToString() + "-" + ProfessionList[ProfessionInt].ToString();
+            //    data.Price = myObject.Next(value, value * 10);
+            //    data.USD = myObject.Next(value, value * 10) * 30;
+            //    data.ImgPath = "/images/MarketPlace/ERNC.png";
+            //    data.Rare = RareList[RareInt].ToString();
+            //    data.Class = ClassList[ClassInt].ToString();
+            //    data.Profession = ProfessionList[ProfessionInt].ToString();
+            //    data.Country = CountryList[CountryInt].ToString();
+            //    data.EndTime = DateTime.Now.AddDays(value);
+            //    data.CalDays = Math.Truncate((DateTime.Now.AddDays(value) - DateTime.Now).TotalDays) + " d "
+            //                   + Math.Truncate(((DateTime.Now.AddDays(value) - DateTime.Now).TotalHours) - Math.Truncate((DateTime.Now.AddDays(value) - DateTime.Now).TotalDays) * 24) + " H ";
+
+            //    if (value % 2 == 0)
+            //    {
+            //        data.IsOpen = true;
+            //    }
+            //    else
+            //    {
+            //        data.IsOpen = false;
+            //    }
+
+
+
+            //    datas.Add(data);
+            //}
             datas = datas.OrderBy(m => m.IsOpen == false).ThenBy(m => m.Number).ToList();
             return PagedList<NFTData>.ToPagedList(datas, PageNumber, PageSize);
         }
@@ -119,7 +183,7 @@ namespace SnakeAsianLeague.Data.Services.MarketPlace
                 int CountryInt = value % RareList.Count();
 
                 NFTData data = new NFTData();
-                data.Number = value;
+                data.Number = value.ToString();
                 data.Name = value.ToString() + "-" + ProfessionList[ProfessionInt].ToString();
                 data.Price = myObject.Next(value, value * 10);
                 data.USD = myObject.Next(value, value * 10) * 30;
@@ -178,6 +242,44 @@ namespace SnakeAsianLeague.Data.Services.MarketPlace
                 datas = datas.OrderBy(m => m.IsOpen == false).ThenBy(m => m.EndTime).ToList();
             }
             return PagedList<NFTData>.ToPagedList(datas, PageNumber, PageSize);
+        }
+
+
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="loginRequest"></param>
+        /// <returns></returns>
+        private async Task<List<NFTRiderUnits>> GetNFTRiderUnits()
+        {
+            var LoginRestRequest = new RestRequest($"NFT/Units");
+            LoginRestRequest.AddHeader("Authorization", Authenticate());
+
+            IRestResponse restResponse = await ServerClient.ExecuteGetAsync(LoginRestRequest);
+            if (restResponse.StatusCode == HttpStatusCode.OK)
+            {
+               // List<NFTRiderUnits> nFTRiders = new List<NFTRiderUnits>();
+
+                return  JsonSerializer.Deserialize<List<NFTRiderUnits>>(restResponse.Content) ??  new List<NFTRiderUnits>(); 
+                
+            }
+            return  new List<NFTRiderUnits>();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private string Authenticate()
+        {
+            string auth = "Unity:Yx2fy5tFfDHAfU7Az";
+            auth = Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(auth));
+            auth = "Basic " + auth;
+            return auth;
         }
     }
 }
