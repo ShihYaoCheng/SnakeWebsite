43edfe4ab4ce149038c40c685379bf92ada623e3
hank <ga895635@gmail.com>
2022-05-20 17:49:21 +0800

    Banner控制在footer上方

--- wwwroot/JavaScript/footerContact.js
+++ wwwroot/JavaScript/footerContact.js
@@ -77,3 +77,24 @@ window.FooterAnimation = function () {
     
    
 }
+
+
+window.Testtest = function () {
+
+    window.addEventListener('scroll', () => {
+        /*可見高度 */
+        var visibleHeight = window.innerHeight
+        var nowHeight = document.documentElement.scrollTop
+        var allHeight = document.body.scrollHeight
+        var footerHeight = document.getElementById("webFooter").clientHeight
+        var SnakeBanner = document.getElementById("SnakeBanner")
+        if (nowHeight + visibleHeight > allHeight - footerHeight) {
+          
+            SnakeBanner.style.bottom = ((nowHeight + visibleHeight - (allHeight - footerHeight)) / visibleHeight * 100).toString() + "%"
+        } else {          
+            SnakeBanner.style.bottom = "0%"
+        }
+    })
+   
+
+}
