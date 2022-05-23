ab891c04c8c87878fceac080b216858790414aa0
ga895635 <ga895635@gmail.com>
2022-05-23 14:07:18 +0800

    點擊nav換頁關閉表單，修改768以下響應式位置

--- wwwroot/JavaScript/footerContact.js
+++ wwwroot/JavaScript/footerContact.js
@@ -1,34 +1,55 @@
-﻿window.FooterContactHide = function () {   
-    var footerContact = document.getElementById("footerContact")     
+﻿
+window.FooterContactHide = function () {
+    var footerContact = document.getElementById("footerContact")
     var contactClose = document.getElementById("contactClose")
     var appreciateGoback = document.getElementById("appreciateGoback")
     var contactContainer = document.getElementById("contactContainer")
     var contact06 = document.getElementById("contact06")
-   
+    var navContainer = document.getElementById("navContainer")
+    var contactPop = document.getElementById("contactPop")  
+
     footerContact.addEventListener('click', (e) => {
         if (e.target.id == "footerContact") {
             closeAction()
-        } 
+        }
     })
-   
+
     contactClose.addEventListener('click', (e) => {
-            closeAction()
+        closeAction()
     })
 
     appreciateGoback.addEventListener('click', (e) => {
         closeAction()
     })
 
+    navContainer.addEventListener('click', (e) => {
+        closeAction()
+    })
+
+
     function closeAction() {
+        
+        contactPop.classList.remove('move')
+        contactPop.classList.add('closureMove')
+        setTimeout(()=> {
         footerContact.style.display = "none"
-        contactContainer.style.display = "flex"
-        contact06.style.display="block"
+                contactContainer.style.display = "flex"
+                contact06.style.display = "block"
+                contactPop.classList.add('move')
+                contactPop.classList.remove('closureMove')
+         }, 500)
     }
 }
 
 window.FooterContactShow = function () {
     var footerContact = document.getElementById("footerContact")
-    footerContact.style.display = "block"
+    var contactPop = document.getElementById("contactPop")
+   
+    contactPop.classList.add('move')
+    setTimeout(() => {
+        footerContact.style.display = "block"
+    }, 1)
+
 }
 
 window.FooterAnimation = function () {
@@ -36,50 +57,50 @@ window.FooterAnimation = function () {
     var contact06 = document.getElementById("contact06")
     var openEnvelop = document.getElementById("openEnvelop")
     var closeEnvelop = document.getElementById("closeEnvelop")
-    var appreciate = document.getElementById("appreciate") 
- 
+    var appreciate = document.getElementById("appreciate")
+
     /*先縮小1.5秒*/
     contact06.classList.add('zoom-out')
-    setTimeout(() => {        
+    setTimeout(() => {
         contact06.style.display = 'none'
         openEnvelop.style.display = 'block'
         openEnvelop.classList.add('zoom-in')
-      
+
     }, 500)
     setTimeout(() => {
-        openEnvelop.classList.remove('zoom-in')       
-        openEnvelop.classList.add('rotate')
+        openEnvelop.classList.remove('zoom-in')
+
         openEnvelop.classList.add('zoom-out')
-   
+
     }, 1000)
     setTimeout(() => {
         openEnvelop.style.display = 'none'
         closeEnvelop.style.display = 'block'
         appreciate.style.display = "block"
-    
+
         contactContainer.classList.add('fly')
         closeEnvelop.classList.add("zoom-in")
         appreciate.classList.add('appreciateFly')
     }, 1500)
     setTimeout(() => {
         closeEnvelop.style.display = 'none'
-        contactContainer.style.display="none"
+        contactContainer.style.display = "none"
 
-       
+
         /*關閉所有動畫*/
         contact06.classList.remove('zoom-out')
-        openEnvelop.classList.remove('rotate')
+
         openEnvelop.classList.remove('zoom-out')
         closeEnvelop.classList.remove('zoom-in')
         appreciate.classList.remove('appreciateFly')
         contactContainer.classList.remove('fly')
     }, 3000)
-    
-   
+
+
 }
 
 
-window.Testtest = function () {
+window.BannerMove = function () {
 
     window.addEventListener('scroll', () => {
         /*可見高度 */
@@ -88,13 +109,15 @@ window.Testtest = function () {
         var allHeight = document.body.scrollHeight
         var footerHeight = document.getElementById("webFooter").clientHeight
         var SnakeBanner = document.getElementById("SnakeBanner")
-        if (nowHeight + visibleHeight > allHeight - footerHeight) {
-          
-            SnakeBanner.style.bottom = ((nowHeight + visibleHeight - (allHeight - footerHeight)) / visibleHeight * 100).toString() + "%"
-        } else {          
-            SnakeBanner.style.bottom = "0%"
+        if (SnakeBanner != null) {
+            if (nowHeight + visibleHeight > allHeight - footerHeight) {
+
+                SnakeBanner.style.bottom = ((nowHeight + visibleHeight - (allHeight - footerHeight)) / visibleHeight * 100).toString() + "%"
+            } else {
+                SnakeBanner.style.bottom = "0%"
+            }
         }
+
     })
-   
 
 }
