35fd41e9bfc0ea9df973c48395113a4b3c0669ae
Reila W <ravennreila@gmail.com>
2022-05-23 10:14:51 +0800

    popup

--- wwwroot/JavaScript/Popup.js
+++ wwwroot/JavaScript/Popup.js
@@ -2,23 +2,35 @@ window.Popup = function () {
     console.log('Popup');
 
     // 編輯姓名
-    $('.products-header .Owned-edit').click(function () { 
+    $('.products-header .Owned-edit').click(function () {
         console.log('.products-header .Owned-edit');
         $('#products-popupblock-Name').addClass('products-popup-open');
-        // e.preventDefault();
     });
 
     // 編輯金額
-    $('#products-Income .Owned-edit').click(function () { 
+    $('#products-Income .Owned-edit').click(function () {
         console.log('.products-header .Owned-edit');
         $('#products-popupblock-AppearanceFee').addClass('products-popup-open');
-        // e.preventDefault();
     });
 
     // 點擊 X 關閉
-    $('.products-popup-close').click(function () { 
+    $('.products-popup-close').click(function () {
         $('.products-popup-bg').removeClass('products-popup-open');
     });
+
+    // var inputTextLength = $('#products-popupblock-Name .products-popup-inputText').val().length;
+    // console.log(inputTextLength);
+    // s$('#products-popupblock-Name .products-popup-inputText').change(function () { 
+    //     if (inputTextLength > 20) {
+    //         $('.products-popup-error').show();
+    //     }
+    //     else {
+    //         $('.products-popup-error').hide();
+    //     }
+        
+    // });
+
+
 
 
 }
