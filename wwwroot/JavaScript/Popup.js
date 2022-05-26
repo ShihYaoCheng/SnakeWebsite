3855f264d957337795f82dd88b24b4241b0d0870
Reila W <ravennreila@gmail.com>
2022-05-26 17:41:37 +0800

    Inventory card

--- wwwroot/JavaScript/Popup.js
+++ wwwroot/JavaScript/Popup.js
@@ -29,26 +29,3 @@ window.Popup = function () {
 
 }
 
-
-// window.ProductsImageMove = function () {
-//     console.log('ProductsImageMove');
-
-//     window.addEventListener('scroll', () => {
-//         /*可見高度 */
-//         var visibleHeight = window.innerHeight
-//         var nowHeight = document.documentElement.scrollTop
-//         var allHeight = document.body.scrollHeight
-//         var footerHeight = document.getElementById("webFooter").clientHeight
-//         var SnakeBanner = document.getElementById("products-header")
-//         if (SnakeBanner != null) {
-//             if (nowHeight + visibleHeight > allHeight - footerHeight) {
-
-//                 SnakeBanner.style.bottom = ((nowHeight + visibleHeight - (allHeight - footerHeight)) / visibleHeight * 100).toString() + "%"
-//             } else {
-//                 SnakeBanner.style.bottom = "0%"
-//             }
-//         }
-
-//     })
-
-// }
