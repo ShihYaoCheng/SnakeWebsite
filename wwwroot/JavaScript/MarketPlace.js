7d06ad23fbaf92262a784cd61fb1ef6007e053a7
Reila W <ravennreila@gmail.com>
2022-05-26 16:20:11 +0800

    Filter range

--- wwwroot/JavaScript/MarketPlace.js
+++ wwwroot/JavaScript/MarketPlace.js
@@ -1,5 +1,5 @@
 window.heartClick = function () {
-    
+
     // $('NFT-Link').on('click', function(e) {
     //     console.log(".NFTcard a")
     //     if (e.target === $(this).find('.heart')[0]) {
@@ -7,95 +7,95 @@ window.heartClick = function () {
     //     }
     // })
 
-    $('.heart, .Filter-heart').click(function(){
+    $('.heart, .Filter-heart').click(function () {
         $(this).toggleClass('heartClickRed')
     })
 }
 window.FilterButtonClick = function () {
-    $('.Filter-Button:not(#Filter-Button-Reset)').click(function(){
+    $('.Filter-Button:not(#Filter-Button-Reset)').click(function () {
         console.log('FilterButtonClick')
         $(this).toggleClass('Filter-Button-Click')
     })
 }
 
 window.FilterArrowSlideClick = function () {
-    $('.Filter-Title').click(function(){
+    $('.Filter-Title').click(function () {
         console.log('FilterArrowSlideClick')
         $(this).parent().children('.Filter-Options').slideToggle('fast');
         $(this).children('.Filter-arrow').toggleClass('Filter-arrow-toggle');
     })
-    $('#Filter-mobile').click(function(){
+    $('#Filter-mobile').click(function () {
         console.log('Filter-mobile-Slide')
-        $('.marketPlaceSidebar').slideToggle('fast'); 
-        $('.Filter-mobile-arrow').toggleClass('Filter-mobile-arrow-toggle'); 
+        $('.marketPlaceSidebar').slideToggle('fast');
+        $('.Filter-mobile-arrow').toggleClass('Filter-mobile-arrow-toggle');
     })
 
 
-    $('.Filter-Web').click(function(){
-        if($(window).width() > 1000){
-            
-                // console.log('Filter > 1000')
-                $('.Filter-Title-Block').addClass('Filter-Web');
-                $('.marketPlaceSidebar-scroll').addClass('marketPlaceSidebar-scroll-Web');
-            
-                $('.marketPlaceSidebar-Web').toggleClass('height-unset'); 
-                $('.marketPlaceSidebar-scroll-Web').slideToggle('fast'); 
-                $('.Filter-web-arrow').toggleClass('Filter-web-arrow-toggle'); 
+    $('.Filter-Web').click(function () {
+        if ($(window).width() > 1000) {
 
-        }else{
-            // console.log('Filter < 1000')
-
-            $('.Filter-Title-Block').removeClass('Filter-Web');
-            $('.marketPlaceSidebar-scroll').removeClass('marketPlaceSidebar-scroll-Web');
-            $('.marketPlaceSidebar-Web').removeClass('height-unset'); 
-
-        }
-    })
-
-
-    $(window).resize(function () { 
-        if($(window).width() > 1000){
-            
             // console.log('Filter > 1000')
             $('.Filter-Title-Block').addClass('Filter-Web');
             $('.marketPlaceSidebar-scroll').addClass('marketPlaceSidebar-scroll-Web');
 
-        }else{
+            $('.marketPlaceSidebar-Web').toggleClass('height-unset');
+            $('.marketPlaceSidebar-scroll-Web').slideToggle('fast');
+            $('.Filter-web-arrow').toggleClass('Filter-web-arrow-toggle');
+
+        } else {
             // console.log('Filter < 1000')
 
             $('.Filter-Title-Block').removeClass('Filter-Web');
             $('.marketPlaceSidebar-scroll').removeClass('marketPlaceSidebar-scroll-Web');
-            $('.marketPlaceSidebar-Web').removeClass('height-unset'); 
+            $('.marketPlaceSidebar-Web').removeClass('height-unset');
+
+        }
+    })
+
+
+    $(window).resize(function () {
+        if ($(window).width() > 1000) {
+
+            // console.log('Filter > 1000')
+            $('.Filter-Title-Block').addClass('Filter-Web');
+            $('.marketPlaceSidebar-scroll').addClass('marketPlaceSidebar-scroll-Web');
+
+        } else {
+            // console.log('Filter < 1000')
+
+            $('.Filter-Title-Block').removeClass('Filter-Web');
+            $('.marketPlaceSidebar-scroll').removeClass('marketPlaceSidebar-scroll-Web');
+            $('.marketPlaceSidebar-Web').removeClass('height-unset');
             $('.marketPlaceSidebar-scroll').show();
         }
     });
 
-    
-        // $('.Filter-Web').click(function(){
 
-        //     if($(window).width() > 1000){
-        //     console.log('Filter > 1000')
-        //     $('.Filter-Title-Block').addClass('Filter-Web');
-        //     $('.marketPlaceSidebar-scroll').addClass('marketPlaceSidebar-scroll-Web');
-            
-        //     $('.marketPlaceSidebar-Web').toggleClass('height-unset'); 
-        //     $('.marketPlaceSidebar-scroll-Web').slideToggle('fast'); 
-        //     $('.Filter-web-arrow').toggleClass('Filter-web-arrow-toggle'); 
-        //     }else{
-        //         console.log('Filter < 1000')
+    // $('.Filter-Web').click(function(){
 
-        //         $('.Filter-Title-Block').removeClass('Filter-Web');
-        //         $('.marketPlaceSidebar-scroll').removeClass('marketPlaceSidebar-scroll-Web');
-        //         $('.marketPlaceSidebar-Web').removeClass('height-unset'); 
-                
-        //     }
+    //     if($(window).width() > 1000){
+    //     console.log('Filter > 1000')
+    //     $('.Filter-Title-Block').addClass('Filter-Web');
+    //     $('.marketPlaceSidebar-scroll').addClass('marketPlaceSidebar-scroll-Web');
 
-        // })
-    
+    //     $('.marketPlaceSidebar-Web').toggleClass('height-unset'); 
+    //     $('.marketPlaceSidebar-scroll-Web').slideToggle('fast'); 
+    //     $('.Filter-web-arrow').toggleClass('Filter-web-arrow-toggle'); 
+    //     }else{
+    //         console.log('Filter < 1000')
 
-    
+    //         $('.Filter-Title-Block').removeClass('Filter-Web');
+    //         $('.marketPlaceSidebar-scroll').removeClass('marketPlaceSidebar-scroll-Web');
+    //         $('.marketPlaceSidebar-Web').removeClass('height-unset'); 
 
- 
+    //     }
+
+    // })
+
+
+
+
+
 
 
 
@@ -103,5 +103,55 @@ window.FilterArrowSlideClick = function () {
 window.updateTextInput = function (val) {
     console.log('rangeInput')
     // $('#textInputRange').val(val);
+}
+
+window.FilterRange = function () {
+    console.log('FilterRange')
+    // var inputMinValue = $('#input-value-lower').val();
+    // var inputMaxValue = parseInt($('#input-value-upper').val());
+
+    var skipSlider = document.getElementById("skipstep");
+    var skipValues = [
+        document.getElementById("skip-value-lower"),
+        document.getElementById("skip-value-upper")
+    ];
+    var MinValue = Number($('#skip-value-lower input').text());
+    var MaxValue = Number($('#skip-value-upper').text());
+
+
+    // $('.textInputRange').keyup(function () { 
+    //     // inputMinValue.text('#input-value-lower');
+    //     var inputMinValue = $('#input-value-lower').val();
+    //     var MinValue = parseInt($('#skip-value-lower').text());
+    //     console.log(inputMinValue)
+
+
+               
+    // });
+
+
+    noUiSlider.create(skipSlider, {
+        start: [MinValue, MaxValue],
+        connect: true,
+        behaviour: "drag",
+        step: 1,
+        range: {
+            min: MinValue,
+            max: MaxValue
+        },
+        format: {
+            from: function (value) {
+                return parseInt(value);
+            },
+            to: function (value) {
+                return parseInt(value);
+            }
+        }
+    });
+
+    skipSlider.noUiSlider.on("update", function (values, handle) {
+        skipValues[handle].innerHTML = values[handle];
+    });
+
 }
 
