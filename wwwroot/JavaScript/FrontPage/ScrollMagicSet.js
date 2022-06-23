3006124d6b773ef70e22d5611d4430210c1e6ba5
hank <ga895635@gmail.com>
2022-06-23 16:07:47 +0800

    about的響應式

--- wwwroot/JavaScript/FrontPage/ScrollMagicSet.js
+++ wwwroot/JavaScript/FrontPage/ScrollMagicSet.js
@@ -1,25 +1,25 @@
 ﻿export function ScrollMagicSet() {
 	//設定start的高度	
-	$(".Start").height($(window).height() + 2000)
+	$(".Start").height($(window).height() + 3000)
 
 	var airshipPath = {
 		entry: {			
 			autoRotate: false,
 			values: [
-				{ x: 100, y:100 },			
-				{ x: 200, y:80},			
-				{ x: 300, y: 150 },
+				{ scale: 1.05 ,x: 100, y:100 },
+				{ scale: 1.1, x: 200, y:80},
+				{ scale: 1.15, x: 300, y: 150 },
 			]
 		},
 		step1: {
 			autoRotate: false,
 			values: [
-				{ scale: 1.1, y: 150 },
-				{ scale: 1.2, y: 100 },
+				{ scale: 1.2, y: 150 },
+				{ scale: 1.25, y: 100 },
 				{ scale: 1.3, y: 150 },
 			]
 		},
-		down: {
+		End: {
 			curviness: 1.25,
 			autoRotate: false,
 			values: [
@@ -28,6 +28,34 @@
 			]
 		}
 	
+	};
+	var QRCodePath = {
+		entry: {
+			autoRotate: false,
+			values: [
+				{ x: 200, y: -90 },
+				{ x: 250, y: -150 },
+				{ x: 300, y: -180 },
+			]
+		},
+	/*	step1: {
+			autoRotate: false,
+			values: [
+				{  y: -175},
+				{ y: -185 },
+				{ y: -180 },
+			]
+		},
+		End: {
+			curviness: 1.25,
+			autoRotate: false,
+			values: [
+				{ y: -175 },
+				{ y: -185 },
+				{ y: -180 },
+			]
+		}
+		*/
 	};
 	var MountPath = {
 		entry: {
@@ -59,7 +87,7 @@
 			autoRotate: false,
 			values: [
 				{ y: -200, x: 5},
-				{ y: -500, x: 0},
+				{ y: -450, x: 0},
 			]
 		},
 		step1: {
@@ -87,37 +115,42 @@
 		End: {
 			autoRotate: false,
 			values: [
-				{ bottom: "0%", y: 0, width: $(window).width() },
+				{ bottom: "-1%", y: 0, width: $(window).width() },
 			]
 		}
 	}
+
 	// create tween
 	var airshipTween = new TimelineMax()
-		.add(TweenMax.to($(".airship"), 2, { css: { bezier: airshipPath.entry }, ease: Power1.easeInOut }))
-		.add(TweenMax.to($(".airship"), 3, { css: { bezier: airshipPath.step1 }, ease: Power1.easeInOut }))
-		.add(TweenMax.to($(".airship"), 2, { css: { bezier: airshipPath.down }, ease: Power1.easeInOut }))
+		.add(TweenMax.to($(".airship"), 3, { css: { bezier: airshipPath.entry }, ease: Power1.easeInOut }))
+		.add(TweenMax.to($(".airship"), 4, { css: { bezier: airshipPath.step1 }, ease: Power1.easeInOut }))
+		.add(TweenMax.to($(".airship"), 10, { css: { bezier: airshipPath.End }, ease: Power1.easeInOut }))
 
+	var QRCodeTween = new TimelineMax()
+		.add(TweenMax.to($(".StartQRcode"), 3, { css: { bezier: QRCodePath.entry }, ease: Power1.easeInOut }))
+	/*	.add(TweenMax.to($(".StartQRcode"), 4, { css: { bezier: QRCodePath.step1 }, ease: Power1.easeInOut }))
+		.add(TweenMax.to($(".StartQRcode"), 10, { css: { bezier: QRCodePath.End }, ease: Power1.easeInOut }))*/
 
 	var MountTween = new TimelineMax()
-		.add(TweenMax.to($(".StartMount"), 2, { css: { bezier: MountPath.entry }, ease: Power1.easeInOut }))
-		.add(TweenMax.to($(".StartMount"), 3, { css: { bezier: MountPath.step1 }, ease: Power1.easeInOut }))
-		.add(TweenMax.to($(".StartMount"), 2, { css: { bezier: MountPath.End }, ease: Power1.easeInOut }))
+		.add(TweenMax.to($(".StartMount"), 3, { css: { bezier: MountPath.entry }, ease: Power1.easeInOut }))
+		.add(TweenMax.to($(".StartMount"), 4, { css: { bezier: MountPath.step1 }, ease: Power1.easeInOut }))
+		.add(TweenMax.to($(".StartMount"), 10, { css: { bezier: MountPath.End }, ease: Power1.easeInOut }))
 
 	var City = new TimelineMax()
-		.add(TweenMax.to($(".StartCity"), 2, { css: { bezier: CityPath.entry }, ease: Power1.easeInOut }))
-		.add(TweenMax.to($(".StartCity"), 3, { css: { bezier: CityPath.step1 }, ease: Power1.easeInOut }))
-		.add(TweenMax.to($(".StartCity"), 2, { css: { bezier: CityPath.End }, ease: Power1.easeInOut }))
+		.add(TweenMax.to($(".StartCity"), 3, { css: { bezier: CityPath.entry }, ease: Power1.easeInOut }))
+		.add(TweenMax.to($(".StartCity"), 4, { css: { bezier: CityPath.step1 }, ease: Power1.easeInOut }))
+		.add(TweenMax.to($(".StartCity"), 10, { css: { bezier: CityPath.End }, ease: Power1.easeInOut }))
 
 	var Cloud = new TimelineMax()
-		.add(TweenMax.to($(".buttonCloud"), 3, { css: { bezier: CloudPath.entry }, ease: Power1.easeInOut }))
-		.add(TweenMax.to($(".buttonCloud"), 4, { css: { bezier: CloudPath.End }, ease: Power1.easeInOut }))
+		.add(TweenMax.to($(".buttonCloud"), 5, { css: { bezier: CloudPath.entry }, ease: Power1.easeInOut }))
+		.add(TweenMax.to($(".buttonCloud"), 12, { css: { bezier: CloudPath.End }, ease: Power1.easeInOut }))
 
 
 	// 製作場景
     var controller = new ScrollMagic.Controller();
 	var airshipScene = new ScrollMagic.Scene({
         triggerElement: ".Start-Container",
-		duration: 2000,		
+		duration: 3000,		
         triggerHook:0     
     })
     //.setClassToggle(".redBlock", "show")  //增加class
@@ -128,7 +161,7 @@
 
 	var MountScene = new ScrollMagic.Scene({
 		triggerElement: ".Start-Container",
-		duration: 2000,
+		duration: 3000,
 		offset: 0,
 		triggerHook: 0
 	})
@@ -137,7 +170,7 @@
 
 	var CityScene = new ScrollMagic.Scene({
 		triggerElement: ".Start-Container",
-		duration: 2000,	
+		duration:3000,
 		triggerHook: 0
 	})		
 		.setTween(City)
@@ -145,15 +178,51 @@
 
 	var CloudScene = new ScrollMagic.Scene({
 		triggerElement: ".Start-Container",
-		duration: 2000,
+		duration: 3000,
 		offset:0,
 		triggerHook:0
 	})	
 		.setTween(Cloud)
 		.addTo(controller);
 
+	var QRCodeScene = new ScrollMagic.Scene({
+		triggerElement: ".Start-Container",
+		
+		offset: 0,
+		triggerHook: 0
+	})
+		.setTween(QRCodeTween)
+		.addTo(controller);
+
 
 
 	
+
+}
+
+
+
+export function AboutReel() {
+
+	// 製作場景
+	var controller = new ScrollMagic.Controller();
+	 AboutReel = new ScrollMagic.Scene({
+		triggerElement: ".reel-Container",		
+		triggerHook: 1,//1,
+		duration: 1000,
+	})		
+		.setClassToggle(".Paper-BG", "reelMove")		
+		.addTo(controller);
+
+	//進入場景後，將場景給關閉
+	AboutReel
+	.on("enter", function (e) {		
+		AboutReel.enabled(false);
+		setTimeout(() => {
+			document.querySelector(".Paper-BG").classList.add("Paper-BG-show")			
+		}, 3000)
+			
+	
+	})
 
 }
