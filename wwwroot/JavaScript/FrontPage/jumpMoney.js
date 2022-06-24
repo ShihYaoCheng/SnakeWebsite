f00b5ef20e5a2a6d27b7b31a022441655086eb1d
hank <ga895635@gmail.com>
2022-06-24 09:50:10 +0800

    跳錢模組完成

---
+++ wwwroot/JavaScript/FrontPage/jumpMoney.js
@@ -0,0 +1,32 @@
+﻿export function jumpMoney( old , newMoney) {
+    console.log("frist", old, newMoney)
+    //第一次設定
+    var unity = (newMoney - old) / 3000
+    var element = document.querySelector('.jumpMomey');
+    var moneyData = {
+        old: old,
+        newMoney: newMoney,
+        unity: unity,
+        element: element,
+    }
+    console.log(moneyData, element)
+    roundJump(moneyData )
+    function roundJump(moneyData) {
+        if (moneyData.old >= moneyData.newMoney) {
+            moneyData.old = moneyData.newMoney
+            moneyData.element.innerHTML = Math.floor(moneyData.old).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+            console.log("last",moneyData.old)
+            return
+        }
+
+        moneyData.old += moneyData.unity
+
+        moneyData.element.innerText = Math.floor(moneyData.old).toString().replace(/\B(?=(\d{3})+(?!\d))/g,',')
+        console.log(moneyData.old)
+
+        setTimeout( () => {
+            roundJump(moneyData)
+        },moneyData.unity )
+
+    }
+}
