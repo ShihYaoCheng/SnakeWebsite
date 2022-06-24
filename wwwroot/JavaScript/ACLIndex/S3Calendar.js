2bc2e9b48c93358211e8b69ef2b390dc0bbdd3d3
hank <ga895635@gmail.com>
2022-06-24 17:17:56 +0800

    修改S3行程表暫存

---
+++ wwwroot/JavaScript/ACLIndex/S3Calendar.js
@@ -0,0 +1,32 @@
+﻿export function S3Calendar() {
+
+        var calendarEl = document.getElementById('S3calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+            navLinks: true,
+            height:1000,
+            headerToolbar: {
+                left: '',
+                center: 'prev,title,next',
+                right:''
+            },
+            events: [{
+                title: '練習',
+                start: '2022-06-07T02:00:00',
+                end: '2022-06-09T14:00:00',
+
+            },
+            {
+                title: '練習a',
+                start: '2022-06-07T02:00:00',
+                end: '2022-06-09T14:00:00',
+          
+            },
+
+            ]
+
+        });
+
+        calendar.render();
+ 
+}
