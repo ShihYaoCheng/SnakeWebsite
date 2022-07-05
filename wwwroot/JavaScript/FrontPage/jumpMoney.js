934206b4bf1b4307bafae49a8b7d4933cb8a875b
hank <ga895635@gmail.com>
2022-07-05 08:56:56 +0800

    跳錢調整

--- wwwroot/JavaScript/FrontPage/jumpMoney.js
+++ wwwroot/JavaScript/FrontPage/jumpMoney.js
@@ -1,39 +1,63 @@
-﻿export function jumpMoney( old , newMoney) {
-  
-    //第一次設定
-    var unity = (newMoney - old) / 300
-    var jumpTime = 1
-    var element = document.querySelector('.jumpMomey');
-    var jumpTimeout 
-    var moneyData = {
-        old: old,
-        newMoney: newMoney,
-        unity: unity,
-        element: element,
-    }
- 
-    roundJump(moneyData )
-    function roundJump(moneyData) {
-        if (moneyData.old >= moneyData.newMoney) {
-            moneyData.old = moneyData.newMoney
-            moneyData.element.innerHTML = Math.floor(moneyData.old).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-            clearTimeout(jumpTimeout)
-            return
+﻿export function jumpMoney(old, newMoney) {
+
+    window.jumpMomey = true
+    var controller = new ScrollMagic.Controller();
+    var jumpMoneyScroll = new ScrollMagic.Scene({
+        triggerElement: ".jumpMomey",
+        triggerHook: 1,//1,
+        duration: 0,
+    })      
+        .addTo(controller);
+
+    //進入場景後，將場景給關閉
+    jumpMoneyScroll
+        .on("enter", function (e) {
+            jumpMoneyScroll.enabled(false);
+            startJump(old, newMoney)
+
+        })
+
+
+    function startJump(old, newMoney) {
+        console.log('1')
+        //第一次設定
+        var unity = (newMoney - old) / 300
+        var jumpTime = 1
+        var element = document.querySelector('.jumpMomey');
+        var jumpTimeout
+        var moneyData = {
+            old: old,
+            newMoney: newMoney,
+            unity: unity,
+            element: element,
         }
 
-        moneyData.old += moneyData.unity
-        if (moneyData.old + moneyData.unity * 15 >= moneyData.newMoney) {          
-            jumpTime += 40
-            if (moneyData.old >= moneyData.newMoney) jumpTime =1
-        }  
-        
-        
-        moneyData.element.innerText = Math.floor(moneyData.old).toString().replace(/\B(?=(\d{3})+(?!\d))/g,',')
- 
+        roundJump(moneyData)
+        function roundJump(moneyData) {
+            if (moneyData.old >= moneyData.newMoney) {
+                moneyData.old = moneyData.newMoney
+                moneyData.element.innerHTML = Math.floor(moneyData.old).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                clearTimeout(jumpTimeout)
+                return
+            }
 
-        jumpTimeout = setTimeout( () => {
-            roundJump(moneyData)
-        }, jumpTime)
+            moneyData.old += moneyData.unity
+            if (moneyData.old + moneyData.unity * 15 >= moneyData.newMoney) {
+                jumpTime += 40
+                if (moneyData.old >= moneyData.newMoney) jumpTime = 1
+            }
 
+
+            moneyData.element.innerText = Math.floor(moneyData.old).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+
+
+            jumpTimeout = setTimeout(() => {
+                roundJump(moneyData)
+            }, jumpTime)
+
+        }
     }
+    
+  
+    
 }
