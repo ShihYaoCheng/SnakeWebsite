746fcdce5f12d757d041b9fc4560045c22e13097
Reila W <ravennreila@gmail.com>
2022-07-15 14:54:35 +0800

    NFTindex_Banner

--- wwwroot/JavaScript/NFTindex/NFTindexBanner.js
+++ wwwroot/JavaScript/NFTindex/NFTindexBanner.js
@@ -1,3 +1,8 @@
 export function NFTindexBanner() {
     console.log('NFTindexBanner')
+    // var scene = document.getElementById('Lantern_block');
+    var scene = document.querySelector('.parallax_block')
+    var parallaxInstance = new Parallax(scene,{
+        selector: '.ParallaxMove'
+    });
 }
