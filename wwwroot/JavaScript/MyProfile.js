430348d860c799ce921188f77d5caa655ca155f4
YuWei <yuwei4chenquan@gmail.com>
2022-07-19 14:15:38 +0800

    metamsak test  use web3

--- wwwroot/JavaScript/MyProfile.js
+++ wwwroot/JavaScript/MyProfile.js
@@ -109,3 +109,135 @@ window.CardAmountLinkDisplay = function (){
 
     });
 }
+
+
+
+
+window.web3button = function () {
+
+
+    console.log("web3button");
+
+
+    //ethereum.networkVersion属性返回MetaMask当前接入的区块链的网络ID。一些常见的值如下：
+
+    //1：以太坊主网
+    //2：Morden测试链
+    //3：Ropsten测试链
+    //4：Rinkeby测试链
+    //42：Kovan测试链
+
+    console.log(ethereum.networkVersion);
+
+    //ethereum.isMetaMask返回true或false，表示当前用户是否安装了MetaMask。
+    console.log('MetaMask installed ? ', ethereum.isMetaMask);
+
+    if (typeof window.ethereum !== 'undefined' || (typeof window.web3 !== 'undefined')) {
+
+        // Web3 browser user detected. You can now use the provider.
+
+        console.log(window.ethereum);
+        console.log(window.web3);
+
+        
+        const provider = window['ethereum'] || window.web3.currentProvider
+
+
+        console.log(provider);
+        try {
+
+
+            console.log(ethereum.isConnected());
+
+
+            //const provider = await detectEthereumProvider();
+
+            //if (provider) {
+            //    startApp(provider); // Initialize your app
+            //} else {
+            //    console.log('Please install MetaMask!');
+            //}
+
+
+
+            ethereum.request({ method: 'eth_requestAccounts' })
+                .then(handleAccountsChanged)
+                .catch((err) => {
+                    if (err.code === 4001) {
+                        // EIP-1193 userRejectedRequest error
+                        // If this happens, the user rejected the connection request.
+                        console.log('Please connect to MetaMask.');
+                    } else {
+                        console.error(err);
+                    }
+                });
+
+
+            //ethereum.request({ method: 'eth_requestAccounts' });
+            //const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+            //const account = accounts[0];
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+    
+       // const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        //ethereum.autoRefreshOnNetworkChange = false;
+        //ethereum.on("accountsChanged",
+        //    function (accounts) {
+        //        DotNet.invokeMethodAsync('Nethereum.Metamask.Blazor', 'SelectedAccountChanged', accounts[0]);
+        //    });
+        //ethereum.on("networkChanged",
+        //    function (networkId) {
+
+        //    });
+      
+
+
+    //const ethereumButton = document.querySelector('.enableEthereumButton');
+
+    //ethereumButton.addEventListener('click', () => {
+    //    //Will Start the metamask extension
+    //    ethereum.request({ method: 'eth_requestAccounts' });
+    //});
+
+    //const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    //const account = accounts[0];
+
+    //console.log(walletAddress);
+    // We currently only ever provide a single account,
+    // but the array gives us some room to grow.
+
+    //const getWeb3 = async () => {
+    //    return new Promise(async (resolve, reject) => {
+    //        const web3 = new Web3(window.ethereum);
+    //        try {
+
+    //            await Window.ethereum.request({ method: "eth_requestAccounts" });
+    //            console.log("7410");
+    //            resolve(web3);
+    //        } catch (error) {
+    //            reject(" 7410");
+    //            reject(error);
+    //        }
+    //    })
+    //};
+
+    //const web3 = await getWeb3();
+    //const walletAddress = await web3.eth.requestAccounts();
+    //const walletBalanceInWei = await web3.eth.getBalance(walletAddress[0]);
+    //const walletBalanceInEth = Math.round(web3.utils.fromWei(walletBalanceInWei) * 1000) / 1000;
+
+
+  
+
+    //target.setAttribute("hidden", "hidden");
+
+    //document.getElementById("wallet_address").innerText = walletAddress;
+    //document.getElementById("wallet_balance").innerText = walletBalanceInEth;
+    //document.getElementById("wallet_info").removeAttribute("hidden");
+
+}
