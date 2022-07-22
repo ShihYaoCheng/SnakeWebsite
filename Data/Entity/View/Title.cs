bacc98723e14564de42adfc40b3d6c918d545a9b
Sam <samsy318417@gmail.com>
2022-07-22 10:02:49 +0800

    新手教學標題

---
+++ Data/Entity/View/Title.cs
@@ -0,0 +1,14 @@
+﻿namespace SnakeAsianLeague.Data.Entity.View
+{
+    public class Title
+    {
+        public string? MainTitle { get; set; }
+        public string? SubTitle { get; set; }
+
+        public Title(string main, string sub)
+        {
+            MainTitle = main;
+            SubTitle = sub;
+        }
+    }
+}
