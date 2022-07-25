78f8f864ffabf5bf9d73b779e84dc7532adcd538
hank <ga895635@gmail.com>
2022-07-25 16:48:01 +0800

    租金部分切版

--- wwwroot/JavaScript/Popup.js
+++ wwwroot/JavaScript/Popup.js
@@ -29,3 +29,67 @@ window.Popup = function () {
 
 }
 
+window.CollectBtnPoP = function () {
+   
+    // 編輯姓名
+    $('.CollectBtn').click(function () {
+        $('.AllCollectPop').addClass('products-popup-open');
+    });
+
+    // 編輯姓名
+    $('.Collect-Btn').click(function (e) {         
+        $('.NFT-Number-Pop')[0].innerText = e.target.parentNode.parentNode.children[0].children[1].innerText
+        $('.SingelAllCollectPop').addClass('products-popup-open');
+    });
+
+    // 點擊 X 關閉
+    $('.products-popup-close').click(function () {
+        $('.products-popup-bg').removeClass('products-popup-open');
+    });
+
+    //點擊 Confirm 關閉
+    $('.products-popup-submit').click(function () {
+        $('.products-popup-bg').removeClass('products-popup-open');
+    });
+  
+
+    // 點擊 背景關閉
+    $(document).mouseup(function (e) {
+        var container = $(".products-popupblock"); // 這邊放你想要排除的區塊
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            $('.products-popup-bg').removeClass('products-popup-open');
+        }
+    });
+
+
+}
+
+//暫放
+window.ShowMSGcheckbox = function () {
+    window.ShowMSGState = 1
+    $('#ShowMSGcheckbox').click(function () {
+        console.log(window.ShowMSGState)
+        window.ShowMSGState = 1 - window.ShowMSGState
+        if (window.ShowMSGState == 1) {
+            $('#ShowMSGcheckbox')[0].src = "/images/MarketPlace/checkbox.webp"
+        } else {
+            $('#ShowMSGcheckbox')[0].src = "/images/MarketPlace/checkbox_checked.webp"
+        }
+       
+        
+    });
+}
+
+/*借放 解決NFT卡片上按鈕點擊問題 */
+window.NFTcardAClick = function () {
+    document.querySelectorAll(".NFTcardA").forEach((e) => {
+        e.addEventListener('click', (e) => {
+            if (e.target.className == "Collect-Btn") {
+                e.preventDefault();
+            }
+        })
+
+    })
+
+
+}
