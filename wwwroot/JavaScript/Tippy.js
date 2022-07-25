c73d03622f20094a2d78bf841031fe9d33beaf67
hank <ga895635@gmail.com>
2022-07-25 08:15:25 +0800

    新增JS，a標籤可包覆button且可被點擊功能，租金切版

--- wwwroot/JavaScript/Tippy.js
+++ wwwroot/JavaScript/Tippy.js
@@ -6,3 +6,17 @@ window.TippyInit = function () {
         // trigger: 'click',
     });
 }
+
+/*借放 解決NFT卡片上按鈕點擊問題 */
+window.NFTcardAClick = function () {
+    document.querySelectorAll(".NFTcardA").forEach((e) => {
+        e.addEventListener('click', (e) => {           
+            if (e.target.className == "Collect-Btn") {
+                e.preventDefault();
+            }
+        })
+
+    })
+
+  
+}
