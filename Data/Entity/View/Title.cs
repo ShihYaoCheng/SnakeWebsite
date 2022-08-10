3dafd6d4b2791e24c63ea16bcbc98da1ca9bef97
hank <ga895635@gmail.com>
2022-08-10 12:01:01 +0800

    修改新手教學程式碼

--- Data/Entity/View/Title.cs
+++ Data/Entity/View/Title.cs
@@ -1,5 +1,50 @@
 ﻿namespace SnakeAsianLeague.Data.Entity.View
 {
+
+   
+
+    public class TeachAllData {
+        public int id { get; set; }
+        public bool shine { get; set; }
+        public string router { get; set; }
+        public AllTilte alltitle { get; set; }
+    
+
+        public TeachAllData(int Id, bool Shine, string Router, string Title, string SubTitle) {
+            id = Id;
+            shine = Shine;
+            router = Router;
+            alltitle = new AllTilte( Title, SubTitle);
+        }
+
+        public class AllTilte
+        {
+            public string title { get; set; }
+            public string subTitle { get; set; }
+
+            public AllTilte(string Title, string SubTitle)
+            {
+                title = Title;
+                subTitle = SubTitle;
+            }
+        }
+
+        public void squareClick(string value)
+        {
+       
+            if (router == value)
+            {
+                shine = true;
+            }
+            else
+            {
+                shine = false;
+            }
+
+        }
+    }
+
+    /*第一版
     public class Title
     {
         public string? MainTitle { get; set; }
@@ -14,8 +59,11 @@
         }
     }
 
+    */
 
-    public class TeachData
+    /*
+     第二版
+      public class TeachData
     {   //方框內的資料
         public string[][]? SquareData { get; set; }        
         //主標題跟副標題
@@ -62,4 +110,6 @@
 
 
     }
+
+     */
 }
