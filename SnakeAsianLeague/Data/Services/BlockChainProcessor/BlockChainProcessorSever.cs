dc40ce7d24024c40c5b85700bcd48e41b00d26b1
YuWei <yuwei4chenquan@gmail.com>
2022-09-02 09:53:35 +0800

    LINE BY USER ID

--- SnakeAsianLeague/Data/Services/BlockChainProcessor/BlockChainProcessorSever.cs
+++ SnakeAsianLeague/Data/Services/BlockChainProcessor/BlockChainProcessorSever.cs
@@ -4,7 +4,7 @@
     {
 
 
-        string UserID = "U39d8c008277c061933705c821020bf68";
+        
         string channelAccessToken = "rEkO8R7f6dQDmdMrobDtiUkaSFbINS5kfKM3ozup4zQGssi3NL4Msak/VtnNKo6LgIv2tCb/VufpihcZ5GADLYiXtjLT5Hz6wYyqsUcK3W7UaIKxVQlo/Ris/rAOwsCikKdcD0p1frf9RKz8bpNJhQdB04t89/1O/w1cDnyilFU=";
 
         public BlockChainProcessorSever()
@@ -14,11 +14,16 @@
 
         public void LineBotMessage(string msg)
         {
+            string UserID = "U39d8c008277c061933705c821020bf68";
             isRock.LineBot.Bot bot = new isRock.LineBot.Bot(channelAccessToken);
             bot.PushMessage(UserID, msg);
         }
 
-
+        public void LineBotMessageByUserID(string UserID , string msg)
+        {
+            isRock.LineBot.Bot bot = new isRock.LineBot.Bot(channelAccessToken);
+            bot.PushMessage(UserID, msg);
+        }
 
     }
 }
