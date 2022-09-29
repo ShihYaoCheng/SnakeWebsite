d6a6a761bab14df5c1ac43b4b3f1095e4b3ad57c
YuWei <yuwei4chenquan@gmail.com>
2022-09-29 14:24:50 +0800

    誤刪檔案

---
+++ SnakeAsianLeague/Data/Services/Backstage/AwardInfoService.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using RestSharp;
+using SnakeAsianLeague.Data.Entity.Backstage;
+using SnakeAsianLeague.Data.Entity.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SnakeAsianLeague.Data.Services.Backstage
+{
+    public class AwardInfoService
+    {
+        private ExternalServers externalServersConfig;
+        private readonly RestClient BackstageServer;
+
+        public AwardInfoService(IOptions<ExternalServers> myConfiguration) 
+        {
+
+            externalServersConfig = myConfiguration.Value;
+            BackstageServer = new RestClient(externalServersConfig.BackstageApiServer );
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<AwardInfo>> GetAwardInfos()
+        {
+            List<AwardInfo> result = new List<AwardInfo>();
+            try
+            {
+                var LoginRestRequest = new RestRequest($"api/Identity/GetAwardInfos");
+                IRestResponse restResponse = await BackstageServer.ExecuteGetAsync(LoginRestRequest);
+                if (restResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    result = Newtonsoft.Json.JsonConvert.DeserializeObject<List<AwardInfo>>(restResponse.Content) ?? new List<AwardInfo>();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("GetAwardInfos : " + ex.Message);
+            }            
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="prizeCode"></param>
+        /// <param name="place"></param>
+        /// <param name="isGuild"></param>
+        /// <param name="station"></param>
+        /// <returns></returns>
+        public async Task<AwardInfo> GetOneAwardInfo(uint userId, string prizeCode, int place, bool isGuild, int station)
+        {
+            AwardInfo result = new AwardInfo();
+            try
+            {
+                var LoginRestRequest = new RestRequest(string.Format("api/Identity/GetOneAwardInfo?userId={0}&prizeCode={1}&place={2}&isGuild={3}&station={4}", userId, prizeCode, place, isGuild, station));
+                IRestResponse restResponse = await BackstageServer.ExecuteGetAsync(LoginRestRequest);
+                if (restResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    result = Newtonsoft.Json.JsonConvert.DeserializeObject<AwardInfo>(restResponse.Content) ?? new AwardInfo();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("GetOneAwardInfo : " + ex.Message);
+            }            
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="prizeCode"></param>
+        /// <param name="place"></param>
+        /// <param name="isGuild"></param>
+        /// <param name="station"></param>
+        /// <returns></returns>
+        public async Task<int> GetOneAwardInfoStatus(uint userId, string prizeCode, int place, bool isGuild, int station)
+        {
+            int result = 0;
+            try
+            {
+                var LoginRestRequest = new RestRequest(string.Format("GetOneAwardInfoStatus?userId={0}&prizeCode={1}&place={2}&isGuild={3}&station={4}", userId, prizeCode, place, isGuild, station));
+                IRestResponse restResponse = await BackstageServer.ExecuteGetAsync(LoginRestRequest);
+                if (restResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    var data = Newtonsoft.Json.JsonConvert.DeserializeObject<string>(restResponse.Content) ?? "0";
+                    result = Convert.ToInt32(data);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("GetOneAwardInfoStatus : " + ex.Message);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="awardInfo"></param>
+        /// <returns></returns>
+        public async Task<bool> InsertAwardInfo(AwardInfo awardInfo)
+        {
+            bool result = false;
+            try
+            {
+                string jsonData = JsonSerializer.Serialize(awardInfo);
+                var request = new RestRequest($"api/Identity/InsertAwardInfo", Method.POST);
+                request.AddJsonBody(jsonData);
+                IRestResponse restResponse = await BackstageServer.ExecuteAsync(request);
+
+                if (restResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("InsertAwardInfo : " + ex.Message);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="awardInfo"></param>
+        /// <returns></returns>
+        public async Task<bool> UpdateAwardInfo(AwardInfo awardInfo)
+        {
+            bool result = false;
+            try
+            {
+                string jsonData = JsonSerializer.Serialize(awardInfo);
+                var request = new RestRequest($"api/Identity/UpdateAwardInfo", Method.POST);
+                request.AddJsonBody(jsonData);
+                IRestResponse restResponse = await BackstageServer.ExecuteAsync(request);
+                if (restResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("UpdateAwardInfo : " + ex.Message);
+            }
+            return result;
+        }
+
+    }
+}
---
+++ SnakeAsianLeague/Data/Services/Backstage/ProfileService.cs
@@ -0,0 +1,193 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using RestSharp;
+using SnakeAsianLeague.Data.Entity;
+using SnakeAsianLeague.Data.Entity.Backstage;
+using SnakeAsianLeague.Data.Entity.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SnakeAsianLeague.Data.Services.Backstage
+{
+    public class ProfileService
+    {
+        //private readonly IDataAccess _db;
+        //private readonly IConfiguration _config;
+
+        private readonly static KeyValuePair<string, string> RequestKey = new KeyValuePair<string, string>("Backend-Auth-Handler", "gmregk343grgeggw[fk55234w46wfwef46gpwekf[43-i@@!#!@#@");
+        
+        private ExternalServers externalServersConfig;
+        private readonly RestClient ServerClient;
+        private readonly RestClient BackstageServer;
+
+        public ProfileService( IOptions<ExternalServers> myConfiguration, HttpClient httpClient)
+        {
+            externalServersConfig = myConfiguration.Value;
+            ServerClient = new RestClient(externalServersConfig.UserServer);
+            BackstageServer = new RestClient(externalServersConfig.BackstageApiServer);
+            Console.WriteLine(externalServersConfig.BackstageApiServer);
+        }
+
+        public async Task<List<Profile>> GetProfiles() 
+        {
+
+            List<Profile> result = new List<Profile>();
+            try
+            {
+                var LoginRestRequest = new RestRequest($"api/Identity/GetProfiles");
+                IRestResponse restResponse = await BackstageServer.ExecuteGetAsync(LoginRestRequest);
+                if (restResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    result = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Profile>>(restResponse.Content) ?? new List<Profile>();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("GetProfiles : " + ex.Message);
+            }
+            return result;
+
+            //string sql = "select * from Profile";
+
+            //return _db.LoadData<Profile, dynamic>(sql, new { }, _config.GetConnectionString("dev"));
+        }
+
+        public async Task<Profile> GetOneProfileByUserId(uint userId)
+        {
+            Profile result = new Profile();          
+            try
+            {
+                var LoginRestRequest = new RestRequest($"api/Identity/GetOneProfileByUserId?userId={userId}");
+                IRestResponse restResponse = await BackstageServer.ExecuteGetAsync(LoginRestRequest);
+                if (restResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    result = Newtonsoft.Json.JsonConvert.DeserializeObject<Profile>(restResponse.Content) ?? new Profile();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("GetOneProfileByUserId : " + ex.Message);
+            }
+            return result;
+        }
+
+        public async Task<bool> InsertProfile(Profile profile)
+        {
+
+            bool result = false;
+            try
+            {
+                string jsonData = JsonSerializer.Serialize(profile);
+                var request = new RestRequest($"api/Identity/InsertProfile", Method.POST);
+                request.AddJsonBody(jsonData);
+                IRestResponse restResponse = await BackstageServer.ExecuteAsync(request);
+
+                if (restResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("InsertProfile : " + ex.Message);
+            }
+
+            return result;
+
+            //string sql = @"insert into Profile (UserId, UserName, Phone, RealName, BeInterviewed , OpenAwardInfo , IdeaOfSnake, Recommendation ,Link , Gender , DateOfBirth, Email, City, SelfIntroduction)
+            //            values (@UserId, @UserName, @Phone, @RealName,@BeInterviewed , @OpenAwardInfo , @IdeaOfSnake, @Recommendation, @Link , @Gender, @DateOfBirth, @Email, @City, @SelfIntroduction);";
+
+            //return _db.SaveData<Profile>(sql, profile, _config.GetConnectionString("dev"));
+        }
+
+        public async Task<bool> UpdateProfile(Profile profile)
+        {
+            bool result = false;
+            try
+            {
+                string jsonData = JsonSerializer.Serialize(profile);
+                var request = new RestRequest($"api/Identity/UpdateProfile", Method.POST);
+                request.AddJsonBody(jsonData);
+                IRestResponse restResponse = await BackstageServer.ExecuteAsync(request);
+
+                if (restResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    result = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("UpdateProfile : " + ex.Message);
+            }
+
+            return result;
+
+
+            //string sql = @"update Profile 
+            //            set UserName = @UserName, Phone = @Phone, RealName = @RealName
+            //                , BeInterviewed = @BeInterviewed, OpenAwardInfo = @OpenAwardInfo, IdeaOfSnake = @IdeaOfSnake, Recommendation = @Recommendation, Link = @Link
+            //                , Gender = @Gender, DateOfBirth = @DateOfBirth , Email = @Email, City = @City , SelfIntroduction = @SelfIntroduction
+            //            where UserId = @UserId;";
+
+            //return _db.SaveData<Profile>(sql, profile, _config.GetConnectionString("dev"));
+        }
+
+
+
+        /// <summary>
+        /// 抓取帳號有擁有 NFT騎士 數量
+        /// </summary>
+        /// <param name="UserID">使用者ID</param>
+        /// <returns></returns>
+        public async Task<int> Get_NFT_Rider_Count(string UserID)
+        {
+            int result = 0;
+            string URL = "/Unit/CheckForBackEnd";
+            var request = new RestRequest(URL, Method.GET);
+            request.AddQueryParameter("UserID", UserID);
+            request.AddHeader("Authorization", Authenticate());
+
+            IRestResponse restResponse = await ServerClient.ExecuteAsync(request);
+            if (restResponse.StatusCode == HttpStatusCode.OK)
+            {
+                RiderList lists = JsonSerializer.Deserialize<RiderList>(restResponse.Content) ?? new RiderList();
+
+                //自有
+                //List<RiderUnit> result = lists.selfUnits.Where(m => m.isNFT == true).ToList() ?? new List<RiderUnit>();
+
+                //租任
+                //List<RiderUnit> result = lists.leaseUnits.Where(m => m.isNFT == true).ToList() ?? new List<RiderUnit>();
+
+                //自有
+                result = lists.selfUnits.Where(m => m.isNFT == true).ToList().Count;
+                return result;
+            }
+            return result;
+
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private string Authenticate()
+        {
+            string auth = "Unity:Yx2fy5tFfDHAfU7Az";
+            auth = Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(auth));
+            auth = "Basic " + auth;
+            return auth;
+        }
+
+
+
+    }
+}
