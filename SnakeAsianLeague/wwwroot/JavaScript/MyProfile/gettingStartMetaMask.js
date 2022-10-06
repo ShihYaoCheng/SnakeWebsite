2fd9b9d3e010a097ac983c2c0474f6cd5f09640b
hank <ga895635@gmail.com>
2022-10-06 14:28:50 +0800

    暫時修改metamask安裝跟登入判定

---
+++ SnakeAsianLeague/wwwroot/JavaScript/MyProfile/gettingStartMetaMask.js
@@ -0,0 +1,16 @@
+﻿export async function gettingStartMetaMask() {
+   
+    if (typeof window.ethereum == 'undefined') {
+        console.log("undefined")
+        alert("please install MetaMask")
+        location.href = '/GettingStarted/create-metamask';
+    } else {
+        const Accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+        console.log(Accounts)
+        if (Accounts[0] != "") {
+            return true
+        } else {
+            return false
+        }       
+    }
+}
