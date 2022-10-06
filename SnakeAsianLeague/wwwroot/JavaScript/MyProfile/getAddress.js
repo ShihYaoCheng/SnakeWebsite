845706e852c1994f8b66d9bea822cbf02dca01e4
hank <ga895635@gmail.com>
2022-10-06 14:11:49 +0800

    暫時解決錢包問題

--- SnakeAsianLeague/wwwroot/JavaScript/MyProfile/getAddress.js
+++ SnakeAsianLeague/wwwroot/JavaScript/MyProfile/getAddress.js
@@ -1,4 +1,6 @@
-﻿export async function  getAddress() {
+﻿export async function getAddress() {
+    //暫放
+    ethereum.request({ method: 'eth_requestAccounts' });
     const web3 = new Web3(Web3.givenProvider)  
     const getAddress = await web3.eth.requestAccounts()
     $("#userAddress")[0].innerText = getAddress[0].slice(0, 5) + "...." + getAddress[0].slice(38)
