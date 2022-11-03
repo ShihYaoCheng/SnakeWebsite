3036b9bf1dc51d4f5188e8c83d27083ec461343e
hank <ga895635@gmail.com>
2022-11-03 10:53:05 +0800

    joinzone縮放

--- SnakeAsianLeague/wwwroot/JavaScript/SnakeIndex/Banner.js
+++ SnakeAsianLeague/wwwroot/JavaScript/SnakeIndex/Banner.js
@@ -7,26 +7,22 @@ export function eventBannerS3Close() {
    
 }
 
-export function BannerMove() {
-    window.addEventListener('scroll', () => {
+export function joinZoneMove() {
+    window.addEventListener('scroll', () => {       
+        if (window.location.pathname != "/") return      
         /*可見高度 */
         var visibleHeight = window.innerHeight
-        var nowHeight = document.documentElement.scrollTop
-        var allHeight = document.body.scrollHeight
-        var footerHeight = document.getElementById("webFooter").clientHeight
-        var SnakeBanner = document.getElementById("SnakeBanner")
-        if (SnakeBanner != null) {
-            if (nowHeight + visibleHeight > allHeight - footerHeight) {
-               
-                SnakeBanner.style.bottom = ((nowHeight + visibleHeight - (allHeight - footerHeight)) / visibleHeight * 100 ).toString() + "%"
-            } else {
-                SnakeBanner.style.bottom = "0%"
-           
-            }
-        }
-
+        var nowHeight = document.documentElement.scrollTop  
+        var NFTindex_BannerHeight = document.getElementById("NFTindex_Banner").scrollHeight    
+        var joinZone = document.querySelector(".join-container")    
+       /* var allHeight = document.body.scrollHeight*/       
+        //這是現在高度 + 螢幕高度 >  BannerHeight區塊高度 + joinZone 的自身高度跟top距離
+        if (nowHeight + visibleHeight > NFTindex_BannerHeight + visibleHeight * 0.14 + joinZone.scrollHeight) {
+            joinZone.classList.add("join-hover")
+        } else {
+            joinZone.classList.remove("join-hover")
+        }       
     })
-
 }
 
 
