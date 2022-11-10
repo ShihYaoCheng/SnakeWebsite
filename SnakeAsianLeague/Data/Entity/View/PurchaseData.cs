fba2b9f14adcc390a40a858bfd1849582c1c57dd
hank <ga895635@gmail.com>
2022-11-10 13:28:24 +0800

    修改marketplace的結構

---
+++ SnakeAsianLeague/Data/Entity/View/PurchaseData.cs
@@ -0,0 +1,9 @@
+﻿namespace SnakeAsianLeague.Data.Entity.View
+{
+    public class PurchaseData
+    {
+        public double gSRCBuy;
+        public decimal gSRCtotal;
+        public string productName;
+    }
+}
