3d94328c03a8f7b286a9d29fd163407a06ec3717
Costa Chen <developerjoy@gmail.com>
2022-11-15 14:51:48 +0800

    The Http location header replace with the target variable.

--- SnakeAsianLeague/RedirectHostRule.cs
+++ SnakeAsianLeague/RedirectHostRule.cs
@@ -14,7 +14,7 @@ public class RedirectHostRule : IRule
         var request = context.HttpContext.Request;
         
         // Console.WriteLine($"Host.Host: {context.HttpContext.Request.Host.Host}, Target.Host: {mTargetUri.Host}, Contain: {context.HttpContext.Request.Host.Host.Contains(mTargetUri.Host)}, {context.HttpContext.Request.Path}");
-        Console.WriteLine($"isSameFQDN: {isSameFQDN()}, isImage: {isImage()}, {context.HttpContext.Request.Path}");
+        // Console.WriteLine($"isSameFQDN: {isSameFQDN()}, isImage: {isImage()}, {context.HttpContext.Request.Path}");
 
         if(isSameFQDN())
             return;
@@ -23,7 +23,8 @@ public class RedirectHostRule : IRule
         {
             context.Result = RuleResult.EndResponse;
             context.HttpContext.Response.StatusCode = StatusCodes.Status302Found;
-            context.HttpContext.Response.Headers.Location = $"https://cdn.origingaia.com/official/global{request.Path}";
+            // context.HttpContext.Response.Headers.Location = $"https://cdn.origingaia.com/official/global{request.Path}";
+            context.HttpContext.Response.Headers.Location = $"{mTargetUri}{request.Path}";
         }
 
         bool isImage() => request.Path.Value != null && request.Path.Value.Contains("images");
