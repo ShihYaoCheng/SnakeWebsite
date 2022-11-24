93245f7416254e358a036d52b7d7e283ed9cc8d7
Reila W <ravennreila@gmail.com>
2022-11-24 14:13:57 +0800

    WarmUpActivities

--- SnakeAsianLeague/wwwroot/JavaScript/i18next.js
+++ SnakeAsianLeague/wwwroot/JavaScript/i18next.js
@@ -13,7 +13,7 @@ window.installI18n = async function () {
     {
       lng: lang ? lang : defaultLang,
       fallbackLng: defaultLang,
-          ns: ["data", "Asian_data", "NFT_data", "Home_data", "ERNC_data", "Started_data", "Battle_data", "Inventory_data","Company_data" ],
+          ns: ["data", "Asian_data", "NFT_data", "Home_data", "ERNC_data", "Started_data", "Battle_data", "Inventory_data","Company_data","WarmUpActivities_data" ],
       defaultNS: "data",
       backend: {
         loadPath: "./Language/{{lng}}/{{ns}}.json",
