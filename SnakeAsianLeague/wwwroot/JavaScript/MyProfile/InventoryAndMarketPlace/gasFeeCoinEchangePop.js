02b13322d2999413656fc35565c0f70fdb6c3da1
hank <ga895635@gmail.com>
2022-12-12 16:09:00 +0800

    新增兩個提示窗

---
+++ SnakeAsianLeague/wwwroot/JavaScript/MyProfile/InventoryAndMarketPlace/gasFeeCoinEchangePop.js
@@ -0,0 +1,13 @@
+﻿export async function gasFeeCoinEchangePop(value) {
+    const web3 = await new Web3(Web3.givenProvider)
+    const getGasPrice = await web3.eth.getGasPrice() / (10 ** 9)
+    console.log("getGasPrice",getGasPrice)
+
+    if (getGasPrice > 150) {
+        return true;
+    }
+    else {
+        return false;
+    }
+    
+}
