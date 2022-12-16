23c029ed19b117fe894c6d23772819ea411c8162
hank <ga895635@gmail.com>
2022-12-16 14:44:09 +0800

    串接網址

---
+++ SnakeAsianLeague/Data/Entity/View/GetUrlView.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Options;
+using SnakeAsianLeague.Data.Entity.Config;
+
+namespace SnakeAsianLeague.Data.Entity.View
+{
+    public class GetUrlView
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        string host { get; set; }
+        public GetUrlView(IOptions<ExternalServers> myConfiguration, IHttpContextAccessor httpContextAccessor)
+        {
+
+            _httpContextAccessor = httpContextAccessor;
+             host = _httpContextAccessor.HttpContext.Request.Host.Value;
+
+
+        }
+
+        public async Task<string> GetUrl(){            
+            return host;
+        }
+    }
+}
--- SnakeAsianLeague/wwwroot/JavaScript/MyProfile/gettingStartMetaMask.js
+++ SnakeAsianLeague/wwwroot/JavaScript/MyProfile/gettingStartMetaMask.js
@@ -1,13 +1,16 @@
-﻿export async function gettingStartMetaMask() {
+﻿export async function gettingStartMetaMask(host) {
 
-    if (navigator.vendor.indexOf('Apple') != -1) location.href = "https://metamask.app.link/dapp/www.ponponsnake.com/GettingStarted/create-metamask";
-    if (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent)) location.href = "https://metamask.app.link/dapp/www.ponponsnake.com/GettingStarted/create-metamask"
+    if (navigator.vendor.indexOf('Apple') != -1) location.href = "https://metamask.app.link/dapp/" + host +"/GettingStarted/create-metamask";
+    if (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent)) location.href = "https://metamask.app.link/dapp/" + host + "/GettingStarted/create-metamask"
 
 
    
     if (typeof window.ethereum == 'undefined') {
         console.log("undefined")
-        alert("please install MetaMask")
+        if (navigator.vendor.indexOf('Apple') == -1 && !(/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent))) {
+            alert("please install MetaMask")
+        }
+       
         //location.href = '/GettingStarted/create-metamask';
     } else {
         const Accounts = await ethereum.request({ method: 'eth_requestAccounts' });
