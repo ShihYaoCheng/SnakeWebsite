f26b6e43552c9019ae94446e5d592dc2d0d6b5cf
hank <ga895635@gmail.com>
2023-01-05 16:32:44 +0800

    中英翻譯

--- SnakeAsianLeague/wwwroot/JavaScript/Festival/NewYear2023/NewYear2023Schedule.js
+++ SnakeAsianLeague/wwwroot/JavaScript/Festival/NewYear2023/NewYear2023Schedule.js
@@ -1,6 +1,10 @@
-﻿window.NewYear2023Schedule = function (value) {  
-    value = JSON.parse(value)
-    console.log(value)
+﻿window.NewYear2023Schedule = function (value, lang) {  
+    value = JSON.parse(value)   
+    let eventsList =[]
+    for (var i in value) {
+        value[i].title = lang == "en" ? value[i].i18n[1] + value[i].title + value[i].i18nFinal[1] : value[i].i18n[0] + value[i].title + value[i].i18nFinal[0]
+        eventsList.push(value[i])
+    } 
     var calendarEl = document.getElementById('NewYear2023Schedule');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
@@ -9,7 +13,7 @@
         initialDate: '2023-01-01', // will be parsed as local
         //navLinks: true,
         height: 1000,
-
+        locale: lang == "en" ? "en-us" : "zh-tw",
         headerToolbar: {
             left: '',
             center: 'prev,title,next',
