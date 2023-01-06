481a4da1f5195bfc6d23f3d30c3a5ded8f7f7144
hank <ga895635@gmail.com>
2023-01-06 11:12:19 +0800

    新增註冊頁面的轉址功能

---
+++ SnakeAsianLeague/wwwroot/JavaScript/ohterCtrl/signUpMobile.js
@@ -0,0 +1,4 @@
+﻿window.signUpMobile = function () {
+    if (navigator.vendor.indexOf('Apple') != -1)window.location.href = "https://apps.apple.com/us/app/%E7%A2%B0%E7%A2%B0%E8%9B%87-2/id1504212624"
+    if (/android|webos|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent)) window.location.href ="https://play.google.com/store/apps/details?id=com.cqigames.snakeknight"
+}
