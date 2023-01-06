a4a8d28f882e7a1170a3cadb2f1a015f8beb21b0
hank <ga895635@gmail.com>
2023-01-06 11:44:14 +0800

    測試ios

--- SnakeAsianLeague/wwwroot/JavaScript/ohterCtrl/signUpMobile.js
+++ SnakeAsianLeague/wwwroot/JavaScript/ohterCtrl/signUpMobile.js
@@ -1,4 +1,9 @@
 ﻿window.signUpMobile = function () {
-    if (navigator.vendor.indexOf('Apple') != -1)window.location.href = "https://apps.apple.com/us/app/%E7%A2%B0%E7%A2%B0%E8%9B%87-2/id1504212624"
-    if (/android|webos|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent)) window.location.href ="https://play.google.com/store/apps/details?id=com.cqigames.snakeknight"
+    console.log("ios", !window.MSStream && /iPad|iPhone|iPod/.test(navigator.userAgent))
+    console.log("android", /android|webos|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent))
+    if (!window.MSStream && /iPad|iPhone|iPod/.test(navigator.userAgent)) {
+        window.location.href = "https://apps.apple.com/us/app/%E7%A2%B0%E7%A2%B0%E8%9B%87-2/id1504212624"
+    } else if (/android|webos|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent)) {
+        window.location.href = "https://play.google.com/store/apps/details?id=com.cqigames.snakeknight"
+    } 
 }
