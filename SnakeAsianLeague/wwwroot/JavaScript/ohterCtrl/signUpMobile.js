9c9f4a877146b242337fcffc785d803fc2f27c49
hank <ga895635@gmail.com>
2023-01-09 15:38:47 +0800

    轉址頁面

--- SnakeAsianLeague/wwwroot/JavaScript/ohterCtrl/signUpMobile.js
+++ SnakeAsianLeague/wwwroot/JavaScript/ohterCtrl/signUpMobile.js
@@ -1,13 +1,13 @@
-﻿window.signUpMobile = function () {
+﻿window.signUpMobile = function (url) {
     console.log("ios", !window.MSStream && /iPad|iPhone|iPod/.test(navigator.userAgent))
     console.log("android", /android|webos|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent))
     if (!window.MSStream && /iPad|iPhone|iPod/.test(navigator.userAgent)) {
         window.location.href = "https://apps.apple.com/us/app/%E7%A2%B0%E7%A2%B0%E8%9B%87-2/id1504212624"
-        return true
+        
     } else if (/android|webos|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent)) {
         window.location.href = "https://play.google.com/store/apps/details?id=com.cqigames.snakeknight"
-        return true
+        
     } else {
-        return false
+        window.location.href = `/SignUp/${url}`
     }
 }
