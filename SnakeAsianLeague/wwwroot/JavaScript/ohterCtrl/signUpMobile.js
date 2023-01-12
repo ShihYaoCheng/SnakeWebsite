f5dfb354c270978bb72beb9c0fdacad623bf71c7
hank <ga895635@gmail.com>
2023-01-12 15:51:26 +0800

    測試從前台fetch資料

--- SnakeAsianLeague/wwwroot/JavaScript/ohterCtrl/signUpMobile.js
+++ SnakeAsianLeague/wwwroot/JavaScript/ohterCtrl/signUpMobile.js
@@ -1,8 +1,8 @@
 ﻿window.signUpMobile = function (code) {
-    //`https://${window.location.host}/InvitationCode/QRcodeSaveInvitation?${new URLSearchParams({ 'code': code})}`
+    //
     //`https://dev.origingaia.com/InvitationCode/QRcodeSaveInvitation?${new URLSearchParams({ 'code': code})}`
     //
-    fetch(`https://dev.origingaia.com/InvitationCode/QRcodeSaveInvitation?${new URLSearchParams({ 'code': code })}`,
+    fetch(`https://${window.location.host}/InvitationCode/QRcodeSaveInvitation?${new URLSearchParams({ 'code': code })}`,
         {
             method: "POST",         
         }
@@ -15,15 +15,18 @@
         })
 
 
+    setTimeout(() => {
+        if (!window.MSStream && /iPad|iPhone|iPod/.test(navigator.userAgent)) {
+            window.location.href = "https://apps.apple.com/us/app/%E7%A2%B0%E7%A2%B0%E8%9B%87-2/id1504212624"
+
+        } else if (/android|webos|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent)) {
+            window.location.href = "https://play.google.com/store/apps/details?id=com.cqigames.snakeknight"
+
+        } else {
+            window.location.href = `/SignUp/${code}`
+        }
+
+    }, 5000)
     
     
-    if (!window.MSStream && /iPad|iPhone|iPod/.test(navigator.userAgent)) {
-        window.location.href = "https://apps.apple.com/us/app/%E7%A2%B0%E7%A2%B0%E8%9B%87-2/id1504212624"
-        
-    } else if (/android|webos|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent)) {
-        window.location.href = "https://play.google.com/store/apps/details?id=com.cqigames.snakeknight"
-        
-    } else {
-        window.location.href = `/SignUp/${code}`
-    }
 }
