9762e6423bb6ee6c3d728d7349cdd5eb8ab9dd10
hank <ga895635@gmail.com>
2023-01-12 15:42:24 +0800

    測試API

--- SnakeAsianLeague/wwwroot/JavaScript/ohterCtrl/signUpMobile.js
+++ SnakeAsianLeague/wwwroot/JavaScript/ohterCtrl/signUpMobile.js
@@ -1,6 +1,22 @@
-﻿window.signUpMobile = function (url) {
-    console.log("ios", !window.MSStream && /iPad|iPhone|iPod/.test(navigator.userAgent))
-    console.log("android", /android|webos|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent))
+﻿window.signUpMobile = function (code) {
+    //`https://${window.location.host}/InvitationCode/QRcodeSaveInvitation?${new URLSearchParams({ 'code': code})}`
+    //`https://dev.origingaia.com/InvitationCode/QRcodeSaveInvitation?${new URLSearchParams({ 'code': code})}`
+    //
+    fetch(`https://dev.origingaia.com/InvitationCode/QRcodeSaveInvitation?${new URLSearchParams({ 'code': code })}`,
+        {
+            method: "POST",         
+        }
+        )
+        .then((response) => {
+            console.log("response", response)
+        })
+        .catch((error) => {
+            console.log("error", error)       
+        })
+
+
+    
+    
     if (!window.MSStream && /iPad|iPhone|iPod/.test(navigator.userAgent)) {
         window.location.href = "https://apps.apple.com/us/app/%E7%A2%B0%E7%A2%B0%E8%9B%87-2/id1504212624"
         
@@ -8,6 +24,6 @@
         window.location.href = "https://play.google.com/store/apps/details?id=com.cqigames.snakeknight"
         
     } else {
-        window.location.href = `/SignUp/${url}`
+        window.location.href = `/SignUp/${code}`
     }
 }
